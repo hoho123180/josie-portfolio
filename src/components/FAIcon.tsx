234ec99321369807f65f48ee547ac2254b5f4e1b
roto93 <roto93@yahoo.com.tw>
2024-09-20 02:53:21 +0800

    :zap: update FAIcon

--- src/components/FAIcon.tsx
+++ src/components/FAIcon.tsx
@@ -4,10 +4,20 @@ import { FC } from 'react'
 
 type Prop = {
   icon: IconDefinition
+  size?: 18 | 20 | 24 | 28 | 30 | 36 | 48
 }
 
-const FAIcon: FC<Prop> = ({ icon }) => {
-  return <FontAwesomeIcon icon={icon} style={{ color: 'currentcolor', width: '1em', height: '1em' }} />
+const FAIcon: FC<Prop> = ({ icon, size }) => {
+  return (
+    <FontAwesomeIcon
+      icon={icon}
+      style={{
+        color: 'currentcolor',
+        width: size ?? '1em',
+        height: size ?? '1em',
+      }}
+    />
+  )
 }
 
 export default FAIcon
