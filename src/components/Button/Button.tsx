a85af283bdf5e5ea157325b0c66c1b4764dbd5ff
roto93 <roto93@yahoo.com.tw>
2024-09-20 16:22:28 +0800

    :zap: update Button: add disabled state

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,6 @@
 import { IconDefinition } from '@fortawesome/fontawesome-svg-core'
 import { FC } from 'react'
+import FAIcon from '../FAIcon'
 import styles from './Button.module.css'
 
 interface Prop {
@@ -8,16 +9,19 @@ interface Prop {
   size?: 'large' | 'medium'
   leadingIcon?: IconDefinition
   trailingIcon?: IconDefinition
+  disabled?: boolean
 }
 
-const Button: FC<Prop> = ({ title, type, size }) => {
+const Button: FC<Prop> = ({ title, type, size, leadingIcon, trailingIcon, disabled }) => {
   return (
     <div
-      className={styles.container}
+      className={`${styles.container} ${disabled ? styles.disabled : ''}`}
       data-type={type ?? 'primary'}
       data-size={size ?? 'large'}
     >
+      {leadingIcon && <FAIcon icon={leadingIcon} size={20} />}
       {title}
+      {trailingIcon && <FAIcon icon={trailingIcon} size={20} />}
     </div>
   )
 }
