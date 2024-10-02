ed4d5ebdc533ab0dfb7810526d7e9fe4a4121ef6
roto93 <roto93@yahoo.com.tw>
2024-10-02 11:13:10 +0900

    :wrench: modify nextjs image config

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  images: { unoptimized: true },
   webpack(config) {
     config.externals.push("pino-pretty", "lokijs", "encoding");
 
