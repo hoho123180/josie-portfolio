da2cc489937e1f7d4805fee4f61752a9e3343924
roto93 <roto93@yahoo.com.tw>
2024-10-07 10:49:30 +0800

    :pencil: update resume link

--- src/configs/links.ts
+++ src/configs/links.ts
@@ -1,4 +1,4 @@
-export const resumeLink = 'https://www.cake.me/pdf/s--UYRvRLhOa6IO3i65gyUk3w--/ZE0m48.pdf'
+export const resumeLink = 'https://www.cake.me/hoho123180'
 export const mediumLink = 'https://josie-chen.medium.com/'
 export const gmailLink = 'https://mail.google.com/mail/?view=cm&fs=1&tf=1&to=hoho123180@gmail.com'
 export const behanceLink = 'https://www.behance.net/josie-chen'
