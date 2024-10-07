87491175d236a533cac0476342a9bf1a817f3a27
roto93 <roto93@yahoo.com.tw>
2024-10-07 11:14:50 +0800

    :art: adjust header and background style

--- src/components/BG/BG.tsx
+++ src/components/BG/BG.tsx
@@ -12,16 +12,16 @@ const BG: FC<Prop> = ({ }) => {
       <Image
         alt='bg-ellipse large'
         src={'/images/bg-L.svg'}
-        width={575 * 2}
-        height={650 * 2}
-        style={{ position: 'absolute', right: '-10%', top: -100, zIndex: -1, opacity: 0.7, overflow: 'visible' }}
+        width={1119}
+        height={1092}
+        style={{ position: 'absolute', right: '-30%', top: -250, zIndex: -1, opacity: 0.7, overflow: 'visible' }}
       />
       <Image
         alt='bg-ellipse small'
         src={'/images/bg-S.svg'}
         width={312.46 * 2}
         height={536 * 2}
-        style={{ position: 'absolute', left: '-10%', top: 350, opacity: 0.7, zIndex: -2, overflow: 'visible' }}
+        style={{ position: 'absolute', left: '-20%', top: 400, opacity: 0.7, zIndex: -2, overflow: 'visible' }}
       />
     </>
   )
