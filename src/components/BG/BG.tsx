578fb7092bffab30c5e72ae29f73bb051029b1c5
roto93 <roto93@yahoo.com.tw>
2024-10-08 17:22:34 +0800

    :art: update background

--- src/components/BG/BG.tsx
+++ src/components/BG/BG.tsx
@@ -1,6 +1,5 @@
-import { FC } from 'react'
-import styles from './BG.module.css'
 import Image from 'next/image'
+import { FC } from 'react'
 
 interface Prop {
 
@@ -11,17 +10,33 @@ const BG: FC<Prop> = ({ }) => {
     <>
       <Image
         alt='bg-ellipse large'
-        src={'/images/bg-L.svg'}
+        src={'/images/bg-L.webp'}
         width={1119}
         height={1092}
-        style={{ position: 'absolute', right: '-30%', top: -250, zIndex: -1, opacity: 0.7, overflow: 'visible' }}
+        style={{
+          position: 'absolute',
+          top: -400,
+          left: '50%',
+          transform: 'translateX(5%)',
+          zIndex: -1,
+          opacity: 1,
+          overflow: 'visible',
+        }}
       />
       <Image
         alt='bg-ellipse small'
-        src={'/images/bg-S.svg'}
-        width={312.46 * 2}
-        height={536 * 2}
-        style={{ position: 'absolute', left: '-20%', top: 400, opacity: 0.7, zIndex: -2, overflow: 'visible' }}
+        src={'/images/bg-S.webp'}
+        width={783 * 1.3}
+        height={852 * 1.3}
+        style={{
+          position: 'absolute',
+          top: 100,
+          left: '50%',
+          transform: 'translateX(-120%)',
+          opacity: 1,
+          zIndex: -2,
+          overflow: 'visible',
+        }}
       />
     </>
   )
