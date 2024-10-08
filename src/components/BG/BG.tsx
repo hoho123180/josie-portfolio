0e5ed733c29875687da3950f1cd10e56d31800ca
roto93 <roto93@yahoo.com.tw>
2024-10-08 17:55:48 +0800

    :art: adjust background gradient position

--- src/components/BG/BG.tsx
+++ src/components/BG/BG.tsx
@@ -7,7 +7,7 @@ interface Prop {
 
 const BG: FC<Prop> = ({ }) => {
   return (
-    <>
+    <div style={{ width: '200%', maxWidth: 2396, minWidth: 1400, position: 'absolute', top: 0, left: '50%', transform: 'translateX(-50%)', zIndex: -1 }}>
       <Image
         alt='bg-ellipse large'
         src={'/images/bg-L.webp'}
@@ -15,9 +15,8 @@ const BG: FC<Prop> = ({ }) => {
         height={1092}
         style={{
           position: 'absolute',
-          top: -400,
-          left: '50%',
-          transform: 'translateX(5%)',
+          top: 'max(-400px, calc(-400px + (1500px - 100vw) * 0.1))',
+          right: '0%',
           zIndex: -1,
           opacity: 1,
           overflow: 'visible',
@@ -30,15 +29,14 @@ const BG: FC<Prop> = ({ }) => {
         height={852 * 1.3}
         style={{
           position: 'absolute',
-          top: 100,
-          left: '50%',
-          transform: 'translateX(-120%)',
+          top: 'max(100px, calc(100px + (1500px - 100vw) * 0.3))',
+          left: '0%',
           opacity: 1,
           zIndex: -2,
           overflow: 'visible',
         }}
       />
-    </>
+    </div>
   )
 }
 
